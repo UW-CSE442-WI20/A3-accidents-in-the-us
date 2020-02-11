3ed1d9305960751e0be0bde2c4d7df92f9e056c0
Nicole Garakanidze <garakani@cs.washington.edu>
2020-02-10 20:11:02 -0800

    initial map of USA visible

--- src/map.js
+++ src/map.js
@@ -17,8 +17,9 @@ var height = 500;
 var svg = d3.select('body')
     .append('svg')
     .attr('width', width)
-    .attr('height', height)
-    .append('g');
+    .attr('height', height);
+
+var g = svg.append('g');
 
 var projection = d3.geoAlbersUsa()
     .translate([width / 2, height / 2])
@@ -27,27 +28,31 @@ var projection = d3.geoAlbersUsa()
 var path = d3.geoPath()
     .projection(projection);
 
+
 var promises = [
+
+    // State topology from: https://github.com/topojson/us-atlas
     // d3.json('https://raw.githubusercontent.com/UW-CSE442-WI20/A3-accidents-in-the-us/master/src/us.json'),
-    d3.json('https://d3js.org/us-10m.v1.json'),
+    //d3.json('https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json'),
+    d3.json('https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json'),
     d3.csv('https://raw.githubusercontent.com/UW-CSE442-WI20/A3-accidents-in-the-us/master/data/general_cords.csv')
 ];
 
 Promise.all(promises).then(ready);
 
 function ready(data) {
-    // console.log(data);
+    console.log(data);
 
     var states = topojson.feature(data[0], data[0].objects.states).features;
-    // console.log(states);
+    console.log(states);
 
-    svg.selectAll('states')
+    g.append('g')
+        .selectAll('class','states')
         .data(states)
-        .enter().append('path')
+        .enter()
+        .append('path')
         .attr('class', 'states')
-        .attr('d', path);
+        .attr('d', path)
+        .attr('fill', 'White')
+        .attr('stroke', 'black');
 }
-
-
-
-
