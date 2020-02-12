e61ba457fb6101d5b0c115f0eae8c50d5de075f0
McKinnon Williams <mck35@cs.washington.edu>
2020-02-12 05:58:45 -0800

    Change data to be integers

--- src/data.js
+++ src/data.js
@@ -1,6 +1,14 @@
 const csvData = require('./WA_Core_Info.csv');
 
 d3.csv(csvData).then(function(data) {
+    data.forEach(function(d) {
+            d.Severity = +d.Severity;
+            d.General_Lat = +d.General_Lat;
+            d.General_Lng = +d.General_Lng;
+            d.General_Time = +d.General_Time;
+            d.General_Tmp = +d.General_Tmp;
+        }
+    );
     console.log(data);
 })
 
